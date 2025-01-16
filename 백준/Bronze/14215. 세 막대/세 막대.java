ec5c20ef2e4397c47b0ab528b75724fda70424cc
jihoon <163746946+hoonZeee@users.noreply.github.com>
2025-01-16 16:03:53 +0900

    [Bronze III] Title: 세 막대, Time: 176 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a + b <= c) {
+            c = a + b - 1;
+            System.out.println(a + b + c);
+        }
+        else if (b + c <= a) {
+            a = b + c - 1;
+            System.out.println(a + b + c);
+        }
+        else if (a + c <= b) {
+            b = a + c - 1;
+            System.out.println(a + b + c);
+        }
+        else
+            System.out.println(a+b+c);
+
+    }
+}
