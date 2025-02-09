fb1957b65dc72b90f339573839fd4e08dae1e526
jihoon <163746946+hoonZeee@users.noreply.github.com>
2025-02-09 16:52:54 +0900

    [Silver IV] Title: 스택 2, Time: 936 ms, Memory: 239036 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+        int total = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < total; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            switch (st.nextToken()){
+                case "1" :
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "2" :
+                   if(stack.isEmpty()) sb.append(-1).append('\n');
+                   else sb.append(stack.pop()).append('\n');
+                   break;
+                case "3" :
+                    sb.append(stack.size()).append('\n');
+                    break;
+                case "4" :
+                    if(stack.isEmpty()) sb.append(1).append('\n');
+                    else sb.append(0).append('\n');
+                    break;
+                case "5" :
+                    if(stack.isEmpty()) sb.append(-1).append('\n');
+                    else sb.append(stack.peek()).append('\n');
+                    break;
+            }
+
+        }
+        System.out.print(sb);
+    }
+
+}
